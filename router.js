44cbe1d04053c0b2f198ff127f40d0ca802e3dd1
Maria <mmorrison1978@gmail.com>
2016-04-09 13:53:45 -0600

    added templates

---
+++ router.js
@@ -0,0 +1,27 @@
+FlowRouter.route('/', {
+  name: 'main',
+  action(){
+    BlazeLayout.render('layout', {child: 'main'});
+  }
+})
+
+FlowRouter.route('/map', {
+  name: 'map',
+  action(){
+    BlazeLayout.render('layout', {child: 'map'});
+  }
+})
+
+FlowRouter.route('/support', {
+  name: 'support',
+  action(){
+    BlazeLayout.render('layout', {child: 'support'});
+  }
+})
+
+FlowRouter.route('/about', {
+  name: 'about',
+  action(){
+    BlazeLayout.render('layout', {child: 'about'})
+  }
+})
