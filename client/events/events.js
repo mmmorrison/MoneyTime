9d7adc6ecc70650ee2d8f2905c7bc30424372f23
Maria <mmorrison1978@gmail.com>
2016-04-10 13:20:02 -0600

    geolocation working

--- client/events/events.js
+++ client/events/events.js
@@ -1,29 +1,57 @@
 if (Meteor.isClient) {
-  Meteor.startup(function() {
-    GoogleMaps.load();
-  });
+    var MAP_ZOOM = 15;
 
-  Template.body.helpers({
-    exampleMapOptions: function() {
-      // Make sure the maps API has loaded
-      if (GoogleMaps.loaded()) {
-        // Map initialization options
-        return {
-          center: new google.maps.LatLng(-37.8136, 144.9631),
-          zoom: 8
-        };
-      }
-    }
-  });
-
-  Template.body.onCreated(function() {
-    // We can use the `ready` callback to interact with the map API once the map is ready.
-    GoogleMaps.ready('exampleMap', function(map) {
-      // Add a marker to the map once it's ready
-      var marker = new google.maps.Marker({
-        position: map.options.center,
-        map: map.instance
-      });
+    Meteor.startup(function() {
+        GoogleMaps.load({
+            key: "AIzaSyCHwkmv_U2MS19Yor_FwdOrfxdk_hDQl4Q"
+        });
     });
-  });
+
+    Template.majorEvents.onCreated(function() {
+        var self = this;
+
+        GoogleMaps.ready('majorEvents', function(map) {
+            var marker;
+
+            // Create and move the marker when latLng changes.
+            self.autorun(function() {
+                var latLng = Geolocation.latLng();
+                if (!latLng)
+                    return;
+
+                // If the marker doesn't yet exist, create it.
+                if (!marker) {
+                    marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(latLng.lat, latLng.lng),
+                        map: map.instance
+                    });
+                }
+                // The marker already exists, so we'll just change its position.
+                else {
+                    marker.setPosition(latLng);
+                }
+
+                // Center and zoom the map view onto the current position.
+                map.instance.setCenter(marker.getPosition());
+                map.instance.setZoom(MAP_ZOOM);
+            });
+        });
+    });
+
+    Template.majorEvents.helpers({
+        geolocationError: function() {
+            var error = Geolocation.error();
+            return error && error.message;
+        },
+        mapOptions: function() {
+            var latLng = Geolocation.latLng();
+            // Initialize the map once we have the latLng.
+            if (GoogleMaps.loaded() && latLng) {
+                return {
+                    center: new google.maps.LatLng(latLng.lat, latLng.lng),
+                    zoom: MAP_ZOOM
+                };
+            }
+        }
+    });
 }
