b10017a445da6f42c380012abe8499176e66c7cd
Maria <mmorrison1978@gmail.com>
2016-04-11 15:33:57 -0600

    changed back to before geocomplete

--- client/map/map.js
+++ client/map/map.js
@@ -1,24 +1,68 @@
 if (Meteor.isClient) {
 
-    Meteor.startup(() => {
+    Meteor.startup(function() {
         GoogleMaps.load({
-            key: 'AIzaSyAK_vkvxDH5vsqGkd0Qn-dDmq-rShTA7UA',
-            libraries: 'places'
+            key: "AIzaSyCHwkmv_U2MS19Yor_FwdOrfxdk_hDQl4Q",
+            libraries: "places"
         });
-        console.log(google);
-
     });
 
+    Template.map.onCreated(function(input) {
+        GoogleMaps.ready('map', function(map) {
+            google.maps.event.addListener(map.instance, 'click', function(event) {
 
-    Template.map.onRendered(function() {
-        this.autorun(() => {
-            if (GoogleMaps.loaded()) {
-                $('#place2').geocomplete({
-                    map: $("#map")
+                Markers.insert({
+                    lat: event.latLng.lat(),
+                    lng: event.latLng.lng()
                 });
-            }
-        });
+            });
 
+            var markers = {};
+
+            Markers.find().observe({
+                added: function(document) {
+                    var marker = new google.maps.Marker({
+                        draggable: true,
+                        animation: google.maps.Animation.DROP,
+                        position: new google.maps.LatLng(document.lat, document.lng),
+                        map: map.instance,
+                        id: document._id
+                    });
+
+                    google.maps.event.addListener(marker, 'dragend', function(event) {
+                        Markers.update(marker.id, {
+                            $set: {
+                                lat: event.latLng.lat(),
+                                lng: event.latLng.lng()
+                            }
+                        });
+                    });
+
+                    markers[document._id] = marker;
+                },
+                changed: function(newDocument, oldDocument) {
+                    markers[newDocument._id].setPosition({
+                        lat: newDocument.lat,
+                        lng: newDocument.lng
+                    });
+                },
+                removed: function(oldDocument) {
+                    markers[oldDocument._id].setMap(null);
+                    google.maps.event.clearInstanceListeners(markers[oldDocument._id]);
+                    delete markers[oldDocument._id];
+                }
+            });
+        });
     });
 
+    Template.map.helpers({
+        mapOptions: function() {
+            if (GoogleMaps.loaded()) {
+                return {
+                    center: new google.maps.LatLng(41.8701095, -87.6706588),
+                    zoom: 4
+                };
+            }
+        }
+    });
 }
--- router.js
+++ router.js
@@ -14,7 +14,7 @@ FlowRouter.route('/map', {
             child: 'map'
         });
     }
-});
+})
 
 
 FlowRouter.route('/majorEvents', {
