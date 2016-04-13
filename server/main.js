6de0938ad678ac6b24a8ba7c579f97a8d02de0ba
Maria <mmorrison1978@gmail.com>
2016-04-13 13:56:22 -0600

    updated

--- server/main.js
+++ server/main.js
@@ -1,3 +1,14 @@
+var connectHandler = WebApp.connectHandlers; // get meteor-core's connect-implementation
+
+// attach connect-style middleware for response header injection
+Meteor.startup(function() {
+    connectHandler.use(function(req, res, next) {
+        res.setHeader('Strict-Transport-Security', 'max-age=2592000; includeSubDomains'); // 2592000s / 30 days
+        return next();
+    })
+})
+
+
 Meteor.publish('users', function() {
     return Meteor.users.find({}, {
         username: 1,
