8fc11bdabef8b607d547ac9eac0352d1776b276b
Maria <mmorrison1978@gmail.com>
2016-04-13 10:51:37 -0600

    updated

--- client/eventList/eventList.js
+++ client/eventList/eventList.js
@@ -13,18 +13,3 @@ Template.eventList.helpers({
             .count()
     }
 });
-
-
-
-Template.eventList.helpers({
-    events: [{
-        _id: 1,
-        name: 'This is task 1'
-    }, {
-        _id: 2,
-        name: 'This is task 2'
-    }, {
-        _id: 3,
-        name: 'This is task 3'
-    }, ],
-});
