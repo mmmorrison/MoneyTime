f5edd7858efe9f3eda6f5dee8ef750669351ab38
Maria <mmorrison1978@gmail.com>
2016-04-13 14:02:32 -0600

    updated

--- server/main.js
+++ server/main.js
@@ -3,7 +3,7 @@ var connectHandler = WebApp.connectHandlers; // get meteor-core's connect-implem
 // attach connect-style middleware for response header injection
 Meteor.startup(function() {
     connectHandler.use(function(req, res, next) {
-        res.setHeader('Strict-Transport-Security', 'max-age=2592000; includeSubDomains'); // 2592000s / 30 days
+        res.setHeader('Access-Control-Allow-Origin', "*"); // 2592000s / 30 days
         return next();
     })
 })
